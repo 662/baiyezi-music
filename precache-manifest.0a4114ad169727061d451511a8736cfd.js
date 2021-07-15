4e3c4868b69e83f34eeef24aa251ccb6ed037c61
662 <662@users.noreply.github.com>
2021-07-15 09:42:58 +0000

    deploy: 662/baiyezi-music@9b8199273336af3dadc2a7205f48af123f832f38

---
+++ precache-manifest.0a4114ad169727061d451511a8736cfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a82847908f62222adb35",
+    "url": "/static/css/main.7470a6f4.chunk.css"
+  },
+  {
+    "revision": "a82847908f62222adb35",
+    "url": "/static/js/main.d35627df.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "663b8996859dd0392377",
+    "url": "/static/js/2.c363ef32.chunk.js"
+  },
+  {
+    "revision": "544efb8d5b7d703a798da139ccdd6611",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/baiyezi-music/precache-manifest.18cad6f21caa4b1110c2d1cfb51ed483.js"
+  "/precache-manifest.0a4114ad169727061d451511a8736cfd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/baiyezi-music/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
